ee5925b6910ae5a2ea4f8ff1f88a44f56a1b1591
clemlak <clementlakhal@gmail.com>
2023-11-17 19:42:41 +0400

    add Foundry

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -4,21 +4,33 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
+interface FeContract {
+    function get() external view returns (uint256);
+}
+
 contract CounterTest is Test {
     Counter public counter;
+    FeContract public fe;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        string[] memory inputs = new string[](4);
+        inputs[0] = "fe";
+        inputs[1] = "build";
+        inputs[2] = "./";
+        inputs[3] = "--overwrite";
+        vm.ffi(inputs);
+        string memory bin = vm.readFile("./output/Main/Main.bin");
+        bytes memory bytecode = vm.parseBytes(bin);
+        address deployed;
+
+        assembly {
+            deployed := create(0, add(bytecode, 0x20), mload(bytecode))
+        }
+
+        fe = FeContract(deployed);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_fe() public {
+        assertEq(fe.get(), 42);
     }
 }
